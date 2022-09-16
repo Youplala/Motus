4bdb900efc1fd6638ab30540fe013e0ee766bda0
Youplala <eliebrosset@gmail.com>
2022-09-16 14:29:05 +0200

    repo cleaning

--- backend/server_haproxy.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const os = require("os");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/os", (req, res) => {
-    res.send("Motus app listening on " + os.hostname() + " port " + port);
-});
