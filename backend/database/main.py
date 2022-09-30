933ff0411c56b778e5d246ba92eb31e4e59ee7d7
Elie Brosset <32967983+Youplala@users.noreply.github.com>
2022-09-30 15:41:51 +0200

    docker connection to db

--- backend/database/main.py
+++ backend/database/main.py
@@ -23,7 +23,7 @@ def auth(user: str = None):
         return {"user": "Not specified"}
     print(f"User: {user}")
     try:
-        conn = http.client.HTTPConnection("localhost:8080")
+        conn = http.client.HTTPConnection("nocodb:8080")
         headers = { 'xc-auth': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfN25iNjR1cWxyMHF0cnQiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJ0b2tlbl92ZXJzaW9uIjoiNGY3YzgwOGU1NTg2YzhhYWFkNTYwZjk3NDZlMDc4Y2U5OGE1ODYwNTU5YTE1MDE5MWEzYjczMzBhYzNjNTUzNTAyNDBlNjAzNWJmMjQxY2YiLCJpYXQiOjE2NjQ1MjQ4MTEsImV4cCI6MTY2NDU2MDgxMX0.zqirz_Oc2wEFAIEDXr6LKRPC-nQwzG4edgGd3tJW0J4" }
 
         conn.request("GET", "/api/v1/db/data/noco/p_zj1dkk4jwhhib9/Motus/views/Motus?offset=0&limit=25&where=", headers=headers)
