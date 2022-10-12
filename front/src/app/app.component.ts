ac6ba6caca8900c759c83948b4a3eb9e20a9ddf7
Utility0 <jul.assuied@gmail.com>
2022-10-12 13:07:07 +0200

    alter logging AuthService

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -6,8 +6,14 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLogged = false
 
   constructor (public _authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.isLogged = this._authService.tokenIsValid
+    
   }
 }
