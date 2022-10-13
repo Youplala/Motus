86155ac7290dc78694a212581c730cf4210be8fd
Elie Brosset <32967983+Youplala@users.noreply.github.com>
2022-10-13 18:25:46 +0200

    fixed sql db init

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,14 +1,14 @@
-CREATE IF NOT EXISTS TABLE auth (
+CREATE TABLE IF NOT EXISTS auth (
     id VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE IF NOT EXISTS TABLE guess (
+CREATE TABLE IF NOT EXISTS guess (
     id VARCHAR(255) NOT NULL,
     guess VARCHAR(30) NOT NULL,
     nb_try INT NOT NULL,
     day timestamp NOT NULL,
-    indice VARCHAR(30) NOT NULL,
+    indice VARCHAR(30) NOT NULL
 );
